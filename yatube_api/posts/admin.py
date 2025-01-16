4b8d9ffe27621aa681fd41964c963f267b939b59
huh6789 <gurgunok@vk.com>
2025-01-16 05:22:43 +0300

    Update admin.py

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Post, Comment, Group, Follow
+from .models import Group, Post, Follow, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
 admin.site.register(Group)
+admin.site.register(Post)
 admin.site.register(Follow)
+admin.site.register(Comment)
